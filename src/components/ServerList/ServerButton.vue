749035cffe339deb6bf748a69733f6c37ced66b5
Marcos Fernandes <marcosanf.c@gmail.com>
2020-11-21 13:35:14 -0400

    componente canal e customizações

---
+++ src/components/ServerList/ServerButton.vue
@@ -0,0 +1,65 @@
+<template>
+  <div 
+    class="server__button"
+    :class="{'server__button-hasNotification': hasNotification, 'server__button-isHome': isHome}">
+
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        selected: Boolean,
+        isHome: Boolean,
+        hasNotification: Boolean,
+        mention: Number
+    }
+}
+</script>
+
+<style scopped lang="scss">
+.server__button {
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  height: 48px;
+  margin-bottom: 8px;
+  background-color: var(--primary);
+  cursor: pointer;
+  position: relative;
+  border-radius: 50%;
+  transition: 0.2s;
+
+  &.active,
+  &:hover {
+      border-radius: 16px;
+      background-color: var(--discord);
+  }
+}
+
+.server__button-isHome {
+    background-color: var(--rocketseat);
+    &:active,
+    &:hover {
+        border-radius: 16px;
+        background-color: var(--rocketseat);
+    }
+}
+
+.server__button-hasNotification {
+    &::before {
+        content: "";
+        width: 9px;
+        height: 9px;
+        background-color: white;
+        position: absolute;
+        left: -17px;
+        top: calc(50% - 4.5px);
+        border-radius: 50%;
+        display: block;
+
+    }
+}
+</style>
---
+++ src/components/ServerList/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+      <ServerButton isHome/>
+      <div class="separator"></div>
+      <ServerButton :mentions="3"/>
+      <ServerButton hasNotification/>
+      <ServerButton :mentions="17" hasNotification/>
+      <ServerButton hasNotification/>
+      <ServerButton />
+      <ServerButton hasNotification :mentions="2"/>
+  </div>
+</template>
+
+<script>
+import ServerButton from './ServerButton'
+export default {
+    components: {
+        ServerButton
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.container{
+    grid-area: SL;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--tertiary);
+    padding: 11px 0;
+    max-height: 100vh;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+    display: none;
+  }
+}
+.separator {
+    width: 32px;
+    border-bottom: solid 2px var(--quaternary);
+    margin-bottom: 8px;
+}
+</style>
